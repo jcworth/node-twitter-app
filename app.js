0611cae4858d863867e5dcb30ebf9a142137a698
Jack Holdsworth <jackch90@gmail.com>
2020-04-24 23:27:43 +0100

    Upload image to tweet

--- app.js
+++ app.js
@@ -4,55 +4,79 @@ const fetch = require('node-fetch');
 global.fetch = fetch;
 const https = require('https');
 const fs = require('fs');
-
 const Twit = require('twit');
 
+// Twitter API authorisation
+const T = new Twit({
+  consumer_key:         process.env.TWITTER_API_KEY,
+  consumer_secret:      process.env.TWITTER_API_SECRET_KEY,
+  access_token:         process.env.ACCESS_TOKEN,
+  access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
+});
+
+// Unsplash API authorisation
 const unsplash = new Unsplash({
   accessKey: `${process.env.UNSPLASH_ACCESS_KEY}`
 });
 
-// console.log(unsplash)
+console.log('Querying Unsplash...')
 
-function saveImageToDisk(photoUrl) {
-  // Create a writable path for the target file, '.pipe()' connects 
-  // readable data to the writeable stream.
-  const file = fs.createWriteStream('./images/file.jpeg')
-  const request = https.get(`${photoUrl}`, function (response) {
-    response.pipe(file)
-  });
-}
-
-unsplash.photos.getRandomPhoto({
-  query: 'brutalism'
+// Search unsplash for a photo, then download it,
+// then attach it to a tweet
+const fetchPhoto = unsplash.photos.getRandomPhoto({
+  query: 'brutalist architecture'
 })
   .then(response => response.json())
-  .then(data => {
-    const photoUrl = data.urls.raw
-    console.log(data);
-    console.log(photoUrl)
-    saveImageToDisk(photoUrl)
+  .then(data => saveImageToDisk(data))
+  .then(data => sendTweet(data))
+  .catch(error => console.log(error))
+
+function saveImageToDisk(data) {
+  // Create a writable path for the target file, '.pipe()' connects 
+  // readable data to the writeable stream.
+  console.log('Saving image to disk...')
+  const file = fs.createWriteStream('./images/file.jpeg')
+  const request = https.get(data.urls.raw, (response) => {
+    response.pipe(file);
   });
+  console.log('Image saved!')
+  return data;
+}
 
-// const T = new Twit({
-//   consumer_key:         process.env.TWITTER_API_KEY,
-//   consumer_secret:      process.env.TWITTER_API_SECRET_KEY,
-//   access_token:         process.env.ACCESS_TOKEN,
-//   access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
-// });
+// Wrapper function for composing a tweet. First uploads
+// the saved image, then writes the text.
+function sendTweet(data) {
+  // console.log(data.urls.raw)
+  // console.log(data.user.links.html)
+  const file = './images/file.jpeg';
+  const params = {
+    encoding: 'base64'
+  }
+  const base64content = fs.readFileSync(file, params);
+  console.log(base64content)
 
+  T.post('media/upload', { media_data: base64content }, uploaded);
 
-// function sendTweet() {
-//   const tweet = {
-//     status: 'Test'
-//   }
-//   function tweetCallback(err, data, response) {
-//     // console.log(data)
-//     if (err) {
-//       console.log('Something went wrong')
-//       console.log(err)
-//     } else {
-//       console.log('Tweet posted')
-//     }
-//   }
-//   T.post('statuses/update', tweet, tweetCallback);
-// }
+  function uploaded(err, data, response) {
+    // Tweet goes here
+    if (err) {
+      console.log(err)
+    } else {
+      const id = data.media_id_string;
+      console.log(id)
+      const status = {
+        status: 'image test',
+        media_ids: [id]
+      }
+      T.post('statuses/update', status, tweetCallback);
+    }
+  }
+
+  function tweetCallback(err, data, response) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('Tweet posted!')
+    }
+  }
+}
