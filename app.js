550ee3012a7412dd458990e7ae339bfca46815f8
Jack Holdsworth <jackch90@gmail.com>
2020-04-24 20:27:59 +0100

    Add unsplash-js library and fetch random photo

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
+const Unsplash = require('unsplash-js').default;
+const fetch = require('node-fetch');
+global.fetch = fetch;
 const Twit = require('twit');
 const Dotenv = require('dotenv').config();
-console.log(process.env.TWITTER_API_KEY)
 
+const unsplash = new Unsplash({
+  accessKey: `${process.env.UNSPLASH_ACCESS_KEY}`
+});
 
-const T = new Twit({
-    consumer_key:         process.env.TWITTER_API_KEY,
-    consumer_secret:      process.env.TWITTER_API_SECRET_KEY,
-    access_token:         process.env.ACCESS_TOKEN,
-    access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
+// console.log(unsplash)
+
+unsplash.photos.getRandomPhoto({
+  query: 'brutalism'
+})
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
   });
 
-const tweet = {
-  status: 'Test'
-}
+const T = new Twit({
+  consumer_key:         process.env.TWITTER_API_KEY,
+  consumer_secret:      process.env.TWITTER_API_SECRET_KEY,
+  access_token:         process.env.ACCESS_TOKEN,
+  access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
+});
 
-function tweetCallback(err, data, response) {
-  console.log(data)
-  if (err) {
-    console.log('Something went wrong')
-  } else {
-    console.log('Tweet posted')
+
+function sendTweet() {
+  const tweet = {
+    status: 'Test'
   }
+  function tweetCallback(err, data, response) {
+    // console.log(data)
+    if (err) {
+      console.log('Something went wrong')
+      console.log(err)
+    } else {
+      console.log('Tweet posted')
+    }
+  }
+  T.post('statuses/update', tweet, tweetCallback);
 }
-
-T.post('statuses/update', tweet, tweetCallback);
